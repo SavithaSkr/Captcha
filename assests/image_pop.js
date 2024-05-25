1d0aff1b20f44cf0bdddd4fde8f0c5dabacbf5bd
SavithaSkr <savitha848410@gmail.com>
2024-05-25 07:47:32 -0400

    commit all pages

---
+++ assests/image_pop.js
@@ -0,0 +1,12 @@
+/* function img_event(event) {
+  var image = event.target.src;
+  document.getElementById("ctr_gallery").style.display = "none";
+  document.getElementById("popup").style.display = "block";
+  document.getElementById("popimg").src = "./assests/images/g1.jpg";
+}
+
+function closehandel() {
+  document.getElementById("ctr_gallery").style.display = "block";
+  document.getElementById("popup").style.display = "none";
+}
+ */
