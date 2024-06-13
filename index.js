4ac9c5390e7d7a38e514457f37dedcbe1105879e
Savitha <130944615+SavithaSkr@users.noreply.github.com>
2024-06-13 12:16:30 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+<<<<<<< HEAD
+var sidenavclose = document.querySelector(".sidenavclose");
+var navbar = document.querySelector(".navbar");
+
+function showNavbar() {
+  navbar.style.left = "0px";
+  sidenavclose.style.left = "25%";
+  console.log("shownav bar");
+}
+function closenavbar() {
+  sidenavclose.style.left = "-100%";
+  navbar.style.left = "-60%";
+  console.log("closenavbar");
+}
+=======
 var sidenavclose = document.querySelector(".sidenavclose");
 var navbar = document.querySelector(".navbar");
 
@@ -9,3 +24,4 @@ function closenavbar() {
   sidenavclose.style.left = "-100%";
   navbar.style.left = "-60%";
 }
+>>>>>>> origin/main
